5baeca3bc0663b743977dc6709c712ba30350049
googologyFan <googology.fan@gmail.com>
2025-07-11 14:39:30 +0900

    全部numにして、calcが引数一つの時num=0の場合にリダイレクト？するようにした

--- lib/NHK2025B_lib/N_PID.h
+++ lib/NHK2025B_lib/N_PID.h
@@ -40,19 +40,20 @@ public:
     NHK2025B_PID() : NHK2025B_PID(PidParameter()) {}
     NHK2025B_PID(const PidParameter &param) { rep(i, NUM_OF_PID_CONTROLLER) pid_data[i].parameter = param; }
     NHK2025B_PID(const PidParameter *&params, int N) { rep(i, N) if (i < NUM_OF_PID_CONTROLLER) pid_data[i].parameter = params[i]; }
-    NHK2025B_PID(std::array<PidParameter, NUM_OF_PID_CONTROLLER> params){ rep(i, NUM_OF_PID_CONTROLLER) pid_data[i].parameter = params[i]; }
+    NHK2025B_PID(std::array<PidParameter, NUM_OF_PID_CONTROLLER> params) { rep(i, NUM_OF_PID_CONTROLLER) pid_data[i].parameter = params[i]; }
 
-    void reset(int j = 0)
+    void reset(int num = 0)
     {
-        pid_data[j].state.i = 0;
-        pid_data[j].state.ie = 0.F;
+        pid_data[num].state.i = 0;
+        pid_data[num].state.ie = 0.F;
     }
 
-    void debug(int j, bool b);
-    void print_debug(int j);
+    void debug(int num, bool b);
+    void print_debug(int num);
 
-    float calc(int j, float e);
-    float getOutput(int j);
+    float calc(int num, float val);
+    float calc(float val) { return calc(0, val); }
+    float getOutput(int num);
 
     void setGoalValue(int num, float val);
     void setProcessValue(int num, float val);
@@ -82,40 +83,40 @@ private:
 
 #include <stdio.h>
 
-float NHK2025B_PID::calc(int j, float val)
+float NHK2025B_PID::calc(int num, float val)
 {
-    float e = val;
+    float e = pid_data[num].cmd.goal_value - (pid_data[num].cmd.process_value = val);
 
-    float Kp = pid_data[j].parameter.kp;
-    float Ki = pid_data[j].parameter.ki;
-    float Kd = pid_data[j].parameter.kd;
-    float offset = pid_data[j].parameter.offset;
-    bool rev = pid_data[j].parameter.reverse;
-    int &i = pid_data[j].state.i;
-    float &ie = pid_data[j].state.ie;
-    float &prev_error = pid_data[j].state.prev_error;
-    float &output = pid_data[j].state.output;
+    float Kp = pid_data[num].parameter.kp;
+    float Ki = pid_data[num].parameter.ki;
+    float Kd = pid_data[num].parameter.kd;
+    float offset = pid_data[num].parameter.offset;
+    bool rev = pid_data[num].parameter.reverse;
+    int &i = pid_data[num].state.i;
+    float &ie = pid_data[num].state.ie;
+    float &prev_error = pid_data[num].state.prev_error;
+    float &output = pid_data[num].state.output;
     if (!i++)
         prev_error = e;
-    ie += e;
+    ie += e + prev_error;
     float ret = offset + Kp * e + Ki * ie + Kd * (e - prev_error);
     prev_error = e;
     output = ret * (rev * -2 + 1);
 
-    return pid_data[j].state.output = output;
+    return pid_data[num].state.output = output;
 }
-float NHK2025B_PID::getOutput(int j = 0) { return pid_data[j].state.output; }
+float NHK2025B_PID::getOutput(int num = 0) { return pid_data[num].state.output; }
 
-void NHK2025B_PID::debug(int j = 0, bool b = true) { pid_data[j].cmd.Isdebug = b; }
+void NHK2025B_PID::debug(int num = 0, bool b = true) { pid_data[num].cmd.Isdebug = b; }
 
-void NHK2025B_PID::print_debug(int j = 0)
+void NHK2025B_PID::print_debug(int num = 0)
 {
     printf("PID | Kp:%f, Ki:%f, Kd:%f, e:%f -> output:%f\n",
-           pid_data[j].parameter.kp,
-           pid_data[j].parameter.ki,
-           pid_data[j].parameter.kd,
-           pid_data[j].state.prev_error,
-           pid_data[j].state.output);
+           pid_data[num].parameter.kp,
+           pid_data[num].parameter.ki,
+           pid_data[num].parameter.kd,
+           pid_data[num].state.prev_error,
+           pid_data[num].state.output);
 }
 
 void NHK2025B_PID::update_ts()
@@ -126,6 +127,6 @@ void NHK2025B_PID::update_ts()
 
 void NHK2025B_PID::setGoalValue(int num, float val) { pid_data[num].cmd.goal_value = val; }
 void NHK2025B_PID::setProcessValue(int num, float val) { pid_data[num].cmd.process_value = val; }
-void NHK2025B_PID::setParameter(int num, PidParameter param){ pid_data[num].parameter = param;}
+void NHK2025B_PID::setParameter(int num, PidParameter param) { pid_data[num].parameter = param; }
 
 #endif // NHK2025B_PID_H
